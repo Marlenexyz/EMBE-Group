d19df04eddba1081cd8cf6d8095d698a8187f21a
LeonardoSchoch <leonardoschoch@gmail.com>
2023-09-07 16:50:00 +0000

    PART 3 outcommented

--- src/main.cpp
+++ src/main.cpp
@@ -19,38 +19,38 @@ Encoder encoder(c1, c2, led);
 int main()
 {
     // --- PART 1/2 ------------------------------------------------------
-    Serial.begin(9600);
-
-    c1.init();
-    c2.init();
-    led.init();
-    encoder.init();
-
-    while(1)
-    {
-        counter = encoder.position();
-        if(counter % 70 == 0 && counter != 0)
-        {
-            Serial.println(encoder.position());
-        }
-    }
-
-    // --- PART 3 --------------------------------------------------------
     // Serial.begin(9600);
 
+    // c1.init();
     // c2.init();
-
-    // DDRD &= ~(1 << DDD2);
-    // PORTD |= (1 << PORTD2);
-    // EICRA |= (1 << ISC01) | (1 << ISC00);
-    // EIMSK |= (1 << INT0);
-    // sei();
+    // led.init();
+    // encoder.init();
 
     // while(1)
     // {
-    //     _delay_ms(10);
-    //     Serial.println(counter);
+    //     counter = encoder.position();
+    //     if(counter % 70 == 0 && counter != 0)
+    //     {
+    //         Serial.println(encoder.position());
+    //     }
     // }
+
+    // --- PART 3 --------------------------------------------------------
+    Serial.begin(9600);
+
+    c2.init();
+
+    DDRD &= ~(1 << DDD2);
+    PORTD |= (1 << PORTD2);
+    EICRA |= (1 << ISC01) | (1 << ISC00);
+    EIMSK |= (1 << INT0);
+    sei();
+
+    while(1)
+    {
+        _delay_ms(10);
+        Serial.println(counter);
+    }
 }
 
 ISR(INT0_vect)
