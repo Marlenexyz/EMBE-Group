732daed8fcf4924f954eab46a499becf57443cbf
JonasDengel <jonas.dengel@web.de>
2023-09-19 12:31:10 +0000

    set speed methode
    set speeed methode in den controller verbaut

--- include/P_controller.h
+++ include/P_controller.h
@@ -1,5 +1,6 @@
 #include <stdint.h>
-#include "encoder.h"
+#include <analog_out.h>
+#include <digital_out.h>
 
 #ifndef P_CONTROLLER_H
 #define P_CONTROLLER_H
@@ -7,14 +8,18 @@
 class P_controller
 {
     public:
-        P_controller(double kp, Encoder& encoder);
+        P_controller(double kp, Analog_out& m1, Digital_out& m2);
                 
         void init();
         double update(double ref, double actual);
+        void setSpeed(float omega);
     
     private:
         double mKp;
-        Encoder& mEncoder;
+
+        static constexpr float maxOmega = 12.5; // rad/s
+        Analog_out mM1;
+        Digital_out mM2;
 
 };
 
