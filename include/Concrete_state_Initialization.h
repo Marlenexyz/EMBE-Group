f2f85a72b7929a38ed4b556d775715d8347e8416
Marlenexyz <143824740+Marlenexyz@users.noreply.github.com>
2023-09-29 15:44:36 +0000

    Assignment 3 Part 1

---
+++ include/Concrete_state_Initialization.h
@@ -0,0 +1,30 @@
+#include <state.h>
+
+#ifndef CONCRETE_STATE_INITIALIZATION_H
+#define CONCRETE_STATE_INITIALIZATION_H
+
+class Concrete_state_Initialization : public State
+{
+public:
+  void on_do() override;
+
+  void on_entry() override;
+
+  void on_exit() override;
+
+  void on_event_reset() override;
+
+  void on_event_set_operational() override;
+  
+  void on_isr_int0() override;
+
+  void on_isr_timer1_compa() override;
+
+  void on_isr_timer1_compb() override;
+
+  void on_isr_timer2_compa() override;
+
+  void on_isr_timer2_compb() override;
+};
+
+#endif
