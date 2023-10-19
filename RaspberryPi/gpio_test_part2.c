60ee04880ff2c37dad1fd06e4dcf47855beab136
Marlenexyz <143824740+Marlenexyz@users.noreply.github.com>
2023-10-19 12:40:37 +0000

    sjdskjhg

---
+++ RaspberryPi/gpio_test_part2.c
@@ -0,0 +1,136 @@
+// an example kernel module using GPIO with interrupts:
+
+
+#include <linux/init.h> 
+#include <linux/module.h> 
+#include <linux/kernel.h> 
+#include <linux/gpio.h>                       // for the GPIO functions 
+#include <linux/interrupt.h>                  // for the IRQ code
+
+MODULE_LICENSE("GPL"); 
+MODULE_AUTHOR("Group 1"); 
+MODULE_DESCRIPTION("A Button/LED test driver for the RPi"); 
+MODULE_VERSION("0.1");
+
+// static unsigned int gpioLED = 17;             // pin 11 (GPIO17) 
+// static unsigned int gpioButton = 27;          // pin 13 (GPIO27) 
+
+static unsigned int gpioC1 = 17;
+static unsigned int gpioC2 = 27;          // pin 13 (GPIO27) 
+
+static unsigned int irqNumber;                // share IRQ num within file 
+static unsigned int numberPresses = 0;        // store number of presses 
+// static bool         ledOn = 0;                // used to invert state of LED
+
+
+// prototype for the custom IRQ handler function, function below 
+static irq_handler_t  erpi_gpio_irq_handler(unsigned int irq, 
+                                            void *dev_id, struct pt_regs *regs);
+
+
+//Funktion für sysfs zum Auslesen der testiable in UserSpace
+static ssize_t number_presses_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%u\n", numberPresses);
+}
+
+// erstellt schreibgeschützten sysfs-Eintrag
+static struct kobj_attribute number_presses_attribute = __ATTR_RO(number_presses);
+
+static struct kobject *test_kobj;
+
+
+static int __init erpi_gpio_init(void) 
+{
+    int result = 0;
+    printk(KERN_INFO "GPIO_TEST: Initializing the GPIO_TEST LKM\n");
+
+    // if (!gpio_is_valid(gpioLED)) 
+    // {
+    //     printk(KERN_INFO "GPIO_TEST: invalid LED GPIO\n");
+    //     return -ENODEV;
+    // }   
+
+    // ledOn = true;
+
+    // gpio_request(gpioLED, "sysfs");          // request LED GPIO
+    // gpio_direction_output(gpioLED, ledOn);   // set in output mode and on 
+    // // gpio_set_value(gpioLED, ledOn);       // not reqd - see line above
+    // gpio_export(gpioLED, false);             // appears in /sys/class/gpio
+    //                                          // false prevents in/out change   
+    gpio_request(gpioC1, "sysfs");       // set up gpioEncoder   
+    gpio_request(gpioC2, "sysfs");       // set up gpioEncoder  
+    gpio_direction_input(gpioC1);        // set up as input   
+    gpio_direction_input(gpioC2);        // set up as input   
+    // gpio_set_debounce(gpioEncoder, 200);      // debounce delay of 200ms
+    gpio_export(gpioC1, false);          // appears in /sys/class/gpio
+    gpio_export(gpioC2, false);          // appears in /sys/class/gpio
+
+    // printk(KERN_INFO "GPIO_TEST: button value is currently: %d\n", 
+    //        gpio_get_value(gpioButton));
+
+    irqNumber = gpio_to_irq(gpioC1);     // map GPIO to IRQ number                          ##Interrupt nur für C1 bisher
+    printk(KERN_INFO "GPIO_TEST: Encoder mapped to IRQ: %d\n", irqNumber);
+
+    // This next call requests an interrupt line   
+    result = request_irq(irqNumber,          // interrupt number requested            
+        (irq_handler_t) erpi_gpio_irq_handler,   // handler function            
+        IRQF_TRIGGER_RISING,                     // on rising edge (press, not release)            
+        "erpi_gpio_handler",                     // used in /proc/interrupts
+        NULL);                                   // *dev_id for shared interrupt lines
+    printk(KERN_INFO "GPIO_TEST: IRQ request result is: %d\n", result);
+
+    // sysfs-Eintrag für numberPresses erstellen
+    test_kobj = kobject_create_and_add("test", kernel_kobj);
+    if (!test_kobj)
+        return -ENOMEM;
+
+    if (sysfs_create_file(test_kobj, &number_presses_attribute.attr)) {
+        printk(KERN_ERR "Cannot create sysfs entry\n");
+        kobject_put(test_kobj);
+        return -ENOMEM;
+    }
+
+
+
+    return result;
+}
+
+static void __exit erpi_gpio_exit(void) 
+{   
+    // printk(KERN_INFO "GPIO_TEST: button value is currently: %d\n", 
+    //        gpio_get_value(gpioButton));
+
+    printk(KERN_INFO "GPIO_TEST: pressed %d times\n", numberPresses);
+    // gpio_set_value(gpioLED, 0);              // turn the LED off
+    // gpio_unexport(gpioLED);                  // unexport the LED GPIO   
+    free_irq(irqNumber, NULL);               // free the IRQ number, no *dev_id   
+    gpio_unexport(gpioC1);               // unexport the Encoder GPIO   
+    gpio_free(gpioC1);                   // free the Button GPIO
+    gpio_unexport(gpioC2);
+    gpio_free(gpioC2);
+    printk(KERN_INFO "GPIO_TEST: Goodbye from the LKM!\n"); 
+}
+    // ## Handler Funktion
+static irq_handler_t erpi_gpio_irq_handler(unsigned int irq, 
+                                           void *dev_id, struct pt_regs *regs) 
+{    
+    if(gpio_get_value(gpioC2))
+    {
+        numberPresses--;
+    }
+    else
+    { 
+        numberPresses++; 
+    }
+      
+    printk(KERN_INFO "numberPresses is %d\n", 
+           numberPresses);
+                        //   global counter
+    return (irq_handler_t) IRQ_HANDLED;      // announce IRQ handled 
+}
+
+module_init(erpi_gpio_init);
+module_exit(erpi_gpio_exit);
+
+
---
+++ RaspberryPi/main.c
@@ -0,0 +1,68 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <stdio.h>
+
+int main()
+{
+    //Enable gpio17
+    int fd_in = open("/sys/class/gpio/export", O_WRONLY);
+    write(fd_in, "17", 2);
+    close(fd_in);
+
+    //Enable gpio22
+    int fd_out = open("/sys/class/gpio/export", O_WRONLY);
+    write(fd_out, "22", 2);
+    close(fd_out);
+
+    //Set gpio17 as input
+    fd_in = open("/sys/class/gpio/gpio17/direction", O_WRONLY);
+    write(fd_in, "in", 2);
+    close(fd_in);
+
+    //Set gpio22 as output
+    fd_out = open("/sys/class/gpio/gpio22/direction", O_WRONLY);
+    write(fd_out, "out", 3);
+    close(fd_out);
+
+    // //Set gpio17 interrupt
+    // fd_in = open("/sys/class/gpio/gpio17/edge", O_WRONLY);
+    // //write(fd_in, "falling", 7);
+    // write(fd_in, "both", 4);
+    // close(fd_in);
+
+    // struct pollfd_in pfd_in;
+    // pfd_in.fd_in = fd_in;
+    // pfd_in.events = POLLPRI;
+
+    while (1)
+    {
+        sleep (0.0001);
+        //Wait for event
+        fd_in = open("/sys/class/gpio/gpio17/value", O_RDONLY);       
+        // int ret = poll(&pfd_in, 1, 3000);
+        char c;
+        read(fd_in, &c, 1);
+        close(fd_in);
+        // if(ret == 0)
+        //     printf("Timeout\n");
+        // else
+            
+        fd_out = open("/sys/class/gpio/gpio22/value", O_WRONLY);
+        write(fd_out, &c, 1);
+        close(fd_out);
+        
+    }
+
+    //Disable gpio17
+    fd_in = open("/sys/class/gpio/unexport", O_WRONLY);
+    write(fd_in, "17", 2);
+    close(fd_in);
+    fd_out = open("/sys/class/gpio/unexport", O_WRONLY);
+    write(fd_out, "22", 2);
+    close(fd_out);
+
+    return(0);
+}
+
+
