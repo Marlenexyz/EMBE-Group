95cf4f8a0afc775d27b770e82c6f03630ef1d653
LeonardoSchoch <leonardoschoch@gmail.com>
2023-11-01 15:00:33 +0000

    motor_control code

--- project5/motor_controller/src/main.cpp
+++ project5/motor_controller/src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int m1 = 5;
+const int m2 = 6;
+char data[4] = {0};
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+	Serial.begin(115200);
+
+	pinMode(m1, OUTPUT);
+	pinMode(m2, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	if (Serial.available() > 0)
+	{
+		Serial.readBytes(data, 4);
+		Serial.println((int)*data);
+
+		analogWrite(m1, atoi(data));
+		digitalWrite(m2, false);
+	}
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
