78bb2398356bde36916a728c6a0049caa1429fd9
Marlenexyz <143824740+Marlenexyz@users.noreply.github.com>
2023-11-09 11:10:30 +0000

    main sensor

--- project5/sensor_controller/src/main.cpp
+++ project5/sensor_controller/src/main.cpp
@@ -1,12 +1,54 @@
 #include <Arduino.h>
-int sensorPin = A0;
+#include "modbus_server.h"
+
+#define uint16_t registerNr= 0;    //RegisterNr anpassen
+
+ModbusServer modbus;
+
+int sensorPin = A0; 
+const int opPin = 'D2';   //D2 - D7
+const int stopPin = 'D3';
+const int preOpPin = 'D4';
+const int comPin = 'D5';
+
 char buffer[10];
-void setup() {
-  Serial.begin(115200);
+
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  modbus.init(0x01, 9600);    //Server Nr, Baudrate
+  pinMode(opPin, INPUT);
+  pinMode(stopPin, INPUT);
+  pinMode(preOpPin, INPUT);
+  pinMode(comPin, INPUT);
 }
 
-void loop() {
+void loop() 
+{
+  // modbus.handleRequest();
+
+  // command = Serial.read();
   int sensorValue = analogRead(sensorPin);
+
   sprintf(buffer,"%d\n",sensorValue);
   Serial.print(buffer);
+  
+  if (digitalRead(opPin))
+  {
+    modbus.setReg(registerNr,0x01)
+  }
+  else if (digitalRead(stopPin))
+  {
+    modbus.setReg(registerNr,0x02)
+  }
+  else if (digitalRead(preOpPin))
+  {
+    modbus.setReg(registerNr,0x80)
+  }
+  else if (digitalRead(comPin)){
+    modbus.setReg(registerNr,0x82)
+  }
+  
 }
